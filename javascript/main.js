b2fa832e9eb4a5a23e481567e422d87679eef6b7
dion kuka <kukadion1@gmail.com>
2020-05-07 22:46:48 +0200

    full commit

--- javascript/main.js
+++ javascript/main.js
@@ -1,42 +1,34 @@
-var Personi= {
-    emer:"arber",
-    mbiemer:"kadriu",
-    mosha:22,
+function shtoLajmin(){
+    var createImg = document.createElement('img');
+    createImg.setAttribute("src", "UBT-LOGO.png");
+    createImg.setAttribute('class','foto-lajmi');
+
+    var createTitle = document.createElement('h3');
+    var titulli = document.getElementById('titulli-new').value;
+    var titulliPermbajtje = document.createTextNode(titulli);
+    createTitle.appendChild(titulliPermbajtje);
+
+    var createParagraf = document.createElement('p');
+    var teksti = document.getElementById('permbajtja-new').value;
+    var tekstiPermbajtje = document.createTextNode(teksti);
+    createParagraf.appendChild(tekstiPermbajtje);
+
+    var createData = document.createElement('h6');
+    var ora = new Date().getHours();
+    var minuta = new Date().getMinutes();
+    var sekonda = new Date().getSeconds();
+    var data = ora+'h:'+minuta+'m:'+sekonda+'s';
+    var tekstiData = document.createTextNode(data);
+    createData.appendChild(tekstiData);
+
+    var divi = document.createElement('div');
+    divi.appendChild(createImg);
+    divi.appendChild(createTitle);
+    divi.appendChild(createParagraf);
+    divi.appendChild(createData);
+    divi.setAttribute('class','form-group');
+
+    var x = document.getElementsByClassName('right-side')[0];
+    x.insertBefore(divi,x.firstChild);
+    document.getElementsByClassName('right-side')[0].removeChild(document.getElementsByClassName('form-group')[4]);
 }
-// console.log(Personi);
-// console.log(Personi.emer);
-
-var Useri={
-    emer:'',
-    mbiemri:'',
-    mosha:'',
-    email:''
-}
-
-window.onload=hello();
-
-var btns;
-function hello(){
-    btns=document.getElementsByTagName('button');
-    console.log(btns)
-    for(var element in btns) {
-        console.log(element);
-    }
-    for(var elem of btns) {
-        console.log(elem);
-    }
-}
-
-function modifyDOM(){
-    var createTitle=document.createElement('h1');
-    var nodeText = document.createTextNode('Ky eshte nje titull');
-    createTitle.appendChild(nodeText);
-    document.getElementsByTagName('body')[0].appendChild(createTitle);
-    var titulli = document.getElementsByTagName('h1')[0];
-
-    //titulli.classList+='titulli';
-    titulli.setAttribute('class','titulli');
-
-
-}
-modifyDOM();
